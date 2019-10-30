feb95fd8501c4ce7b50e2bfc9b87b97e0a0d1242
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2019-10-29 22:07:52 -0300

    Update Solicitar conexão Python e MySQL.py

--- Index/Solicitar conexão Python e MySQL.py
+++ Index/Solicitar conexão Python e MySQL.py
@@ -1,9 +1,9 @@
 '''
 @Autor: Paulo Alcântara https://github.com/alpdias
 '''
-import FuncaoDeConexao # Biblioteca com a função de conexão entre Python e o MySQL.
+import Funcao # Biblioteca com a função de conexão entre Python e o MySQL.
 
-cursor = FuncaoDeConexao.RetornarConexaoMySQL() # Recebe a conexão MySQL.
+cursor = Funcao.RetornarConexaoMySQL() # Recebe a conexão MySQL.
 resultado = cursor.execute('SELECT * FROM pessoas') # Recebe a solicitação dentro do MySQL.
 for resultado in cursor: # Mostra o resultado da solicitação.
     print(resultado)
