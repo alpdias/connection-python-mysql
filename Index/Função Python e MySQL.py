7eaf5a0152f3944dfe50cc465543bf1cc3b2f99e
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2019-10-30 23:52:28 -0300

    Update Função Python e MySQL.py

--- Index/Função Python e MySQL.py
+++ Index/Função Python e MySQL.py
@@ -1,7 +1,18 @@
 '''
 @Autor: Paulo Alcântara https://github.com/alpdias
 '''
-import Função # Biblioteca com a função de conexão entre Python e o MySQL.
+# Função para realizar conexão com o banco de dados MySQL e o código Python.
+def RetornarConexaoMySQL():
+    import pymysql # Biblioteca de conexão entre o servidor do MySQL e o Python.
+    conexao = pymysql.connect( # Variável que recebe as configurações de conexão.
+        host = 'localhost', # Servidor.
+        database = 'python', # Banco de dados.
+        user = 'root', # Usuário.
+        passwd = '', # Senha.
+    )
+    cursor = conexao.cursor() # Recebe a conexão MySQL.
+    return cursor
+
 
 cursor = Função.RetornarConexaoMySQL() # Recebe a conexão MySQL.
 resultado = cursor.execute('SELECT * FROM pessoas') # Recebe a solicitação dentro do MySQL.
