f0912a82ebf7a60bb9db08c7a7a482d89979653c
Paulo <al.pdias24@gmail.com>
2020-01-01 13:18:31 -0300

    update python-mysql

--- src/python-mysql.py
+++ src/python-mysql.py
@@ -1,6 +1,7 @@
 '''
 @Autor: Paulo https://github.com/alpdias
 '''
+
 # Função para realizar a conexão com um código Python e um banco de dado MySQL.
 def retornarConexao():
     import pymysql # Biblioteca de conexão entre o servidor do MySQL e o Python.
@@ -16,6 +17,7 @@ def retornarConexao():
 
 cursor = retornarConexao() # Recebe a conexão MySQL dentro da função.
 resultado = cursor.execute('SELECT * FROM pessoas') # Recebe a solicitação dentro do MySQL para vizualizar a tabela.
+
 print('')
 for resultado in cursor: # Mostra o resultado da solicitação em linhas.
     print(resultado)
